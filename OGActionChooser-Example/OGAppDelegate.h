6ec0e84ac7b6b211da5ec3e18dc99ec2704b99c9
Oleg Geier <relikd@me.com>
2012-04-12 15:42:07 +0200

    Initial Commit

---
+++ OGActionChooser-Example/OGAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  OGAppDelegate.h
+//  OGActionChooser-Example
+//
+//  Created by ich on 12.04.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class OGViewController;
+
+@interface OGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) OGViewController *viewController;
+
+@end
---
+++ OGActionChooser-Example/OGViewController.h
@@ -0,0 +1,13 @@
+//
+//  OGViewController.h
+//  OGActionChooser-Example
+//
+//  Created by ich on 12.04.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OGViewController : UIViewController
+
+@end
